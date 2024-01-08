cc79c193c08263c6245f21079e361f493e63c0d9
Suri Hui <suri.hui@mrjaffesclass.com>
2024-01-08 11:49:31 -0800

    work on UI

---
+++ TicTacToe/UserInterface.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+/**
+ * Write a description of class UserInterface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class UserInterface
+{
+    
+    
+    public void run() {
+        Scanncer scr = new Scanner(System.in);
+        while(true) {
+           if (scanner.hasNext()) {
+               String str = scanner.next();
+           }
+        }
+    }
+    
+    public String getUserName() {
+        return 
+    }
+}
